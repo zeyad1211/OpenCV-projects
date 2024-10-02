1a286e3d51382a803d30438ddd40dbcdd2932469
Zeyad Abdelhalim <zeyadwagih@gmail.com>
2024-10-02 18:14:56 +0400

    Delete tempCodeRunnerFile.py

--- tempCodeRunnerFile.py
+++
@@ -1,6 +0,0 @@
-       # if myData in myDataList:
-        #     status = 'authorized'
-        #     color = (0, 255, 0)
-        # else:
-        #     status = 'un-authorized'
-        #     color = (0, 0, 255)
